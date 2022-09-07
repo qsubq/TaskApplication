63f6173c9c6dd16acae5c3bebd9d3facfaf3f67e
FataLity2328 <maksim-kargin@bk.ru>
2022-09-07 08:36:43 +0300

    Ui part and view model

--- app/src/main/java/com/github/qsubq/taskapplication/app/presentation/screen/tasks/TaskFragment.kt
+++ app/src/main/java/com/github/qsubq/taskapplication/app/presentation/screen/tasks/TaskFragment.kt
@@ -1,19 +1,132 @@
 package com.github.qsubq.taskapplication.app.presentation.screen.tasks
 
+import android.graphics.Color
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Space
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.github.qsubq.taskapplication.R
 import com.github.qsubq.taskapplication.databinding.FragmentTaskBinding
 
 class TaskFragment : Fragment() {
     private lateinit var binding: FragmentTaskBinding
+    private val viewModel: TaskViewModel by viewModels()
+    private var hourHeight = 240.0
+    private var dealWidth = 40
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private var selectedDate = Calendar.getInstance()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentTaskBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            selectedDate.set(year, month, dayOfMonth)
+            update()
+        }
+        update()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun update() {
+
+        viewModel.loadData(selectedDate)
+        val dealsColumns: LinearLayout = binding.dealsColumns
+        dealsColumns.removeAllViews()
+
+        val hours = LinearLayout(this.context)
+        val lph = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT)
+        lph.setMargins(20, 3, 10, 0)
+        hours.layoutParams = lph
+        hours.orientation = LinearLayout.VERTICAL
+
+        for (i in 0..23) {
+            val hour = TextView(this.context)
+            val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT)
+            lp.setMargins(0, 0, 0, 0)
+            hour.setBackgroundColor(Color.CYAN)
+            hour.layoutParams = lp
+
+            hour.text = convertIntToTime(i)
+            hour.setTextColor(Color.BLACK)
+            hour.layoutParams.height = hourHeight.toInt()
+            hour.layoutParams.width = 120
+            hours.addView(hour)
+        }
+        dealsColumns.addView(hours)
+
+
+        if (viewModel.tasksLiveData.value?.size != 0) {
+            for (i in viewModel.tasksLiveData.value!!) {
+
+                val dealColumn = LinearLayout(this.context)
+                val lpd = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT)
+                lpd.setMargins(0, 3, 5, 0)
+                dealColumn.layoutParams = lpd
+                dealColumn.orientation = LinearLayout.VERTICAL
+
+                val block = Space(this.context)
+                block.setBackgroundColor(Color.MAGENTA)
+                block.layoutParams =
+                    LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT)
+                block.layoutParams.height = (hourHeight / 60 * i.timeStart).toInt()
+                block.layoutParams.width = dealWidth
+
+                dealColumn.addView(block)
+
+                val deal = TextView(this.context)
+                val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT)
+                lp.setMargins(0, 0, 0, 0)
+                this.context?.let { ContextCompat.getColor(it, R.color.teal_lite) }
+                    ?.let { deal.setBackgroundColor(it) }
+                deal.layoutParams = lp
+                deal.setPadding(15, 0, 0, 0)
+//                if (i.description != "") deal.text = i.name + "\n--------\n(" + i.description + ")"
+                if (i.description != "") deal.text = getString(R.string.task_desc,i.name,i.description)
+                else deal.text = i.name
+                deal.setTextColor(Color.BLACK)
+                deal.layoutParams.height = (hourHeight / 60 * (i.timeFinish - i.timeStart)).toInt()
+                deal.layoutParams.width = dealWidth
+                dealColumn.addView(deal)
+
+                val block2 = Space(this.context)
+                block2.setBackgroundColor(Color.MAGENTA)
+                block2.layoutParams =
+                    LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT)
+                block2.layoutParams.height = (hourHeight / 60 * (24 * 60 - i.timeFinish)).toInt()
+                block2.layoutParams.width = dealWidth
+
+                dealColumn.addView(block2)
+                dealsColumns.addView(dealColumn)
+
+            }
+        }
+    }
+
+    private fun convertIntToTime(i: Int): String {
+        return if (i < 10) "0$i:00" else "$i:00"
     }
 }
--- app/src/main/java/com/github/qsubq/taskapplication/app/presentation/screen/tasks/TaskViewModel.kt
+++ app/src/main/java/com/github/qsubq/taskapplication/app/presentation/screen/tasks/TaskViewModel.kt
@@ -1,12 +1,43 @@
 package com.github.qsubq.taskapplication.app.presentation.screen.tasks
 
-import androidx.lifecycle.LiveData
+import android.app.Application
+import android.icu.util.Calendar
+import android.os.Build
+import android.text.format.DateUtils
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.github.qsubq.taskapplication.data.db.TaskModel
 import io.realm.Realm
-import java.util.*
 
-class TaskViewModel: ViewModel() {
-    private var realm: Realm = Realm.getDefaultInstance()
+class TaskViewModel(application: Application) : AndroidViewModel(application) {
+    private val realm: Realm = Realm.getDefaultInstance()
+    var tasksLiveData = MutableLiveData<MutableList<TaskModel>>()
+    private val context = application
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun loadData(selectedDate: Calendar) {
+
+        if (realm.where(TaskModel::class.java).findAll().size != 0) {
+            tasksLiveData.value?.clear()
+            for (i in realm.where(TaskModel::class.java).findAll()
+                .filter { d -> dealInSelectedDay(d,selectedDate) }) {
+                tasksLiveData.value?.add(i)
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun dealInSelectedDay(d: TaskModel, selectedDate: Calendar): Boolean {
+        val dealDate: String? = d.date
+        val selectedDateStr: String? = DateUtils.formatDateTime(
+            context,
+            selectedDate.timeInMillis,
+            DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_SHOW_YEAR
+        )
+        if (dealDate != null) {
+            return dealDate == selectedDateStr
+        }
+        return false
+    }
 }
